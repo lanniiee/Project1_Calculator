0c5c58704ae3913ce657b8b40970ace58431870c
lanniiee <lav250397@hotmail.com>
2022-09-30 09:16:59 +0100

    initial commit

---
+++ project_calculator/app.js
@@ -0,0 +1,10 @@
+// Adding Element
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+
+// Functions
+
+
+
+// AddEventListener 
