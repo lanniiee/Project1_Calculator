118c0ab2ef55ca4ea2a7d7604d07638aa2d881d4
lanniiee <lav250397@hotmail.com>
2022-09-30 14:44:23 +0100

    change display

--- project_calculator/app.js
+++ project_calculator/app.js
@@ -4,12 +4,21 @@ const keys = calculator.querySelector(".calculator__keys");
 const display = document.querySelector(".calculator__display");
 
 // Functions
+
+//function for key click. 
 const keyClick = (event) => {
     if (event.target.matches("button")) {
         const key = event.target;
         const action = key.dataset.action;
-        if (!action) {
+        const content = key.innerText;
+        const numberDisplayed = display.innerText; 
+        if (!action) {    //
             console.log("number key!");
+            if (numberDisplayed === "0") {
+                display.innerText = content;
+            } else {
+                display.innerText = numberDisplayed+content;
+            }
         } else if (action === "addition" || action === "subtraction" || action === "multiplication" || action === "division") {
             console.log("operator keys!");
         } else if (action === "decimal!") {
@@ -23,16 +32,6 @@ const keyClick = (event) => {
         } else if (action === "negativepositive") {
             console.log("Negative key!")
         }
-    }
-}
-
-const calculatorDisplay = (event) => {
-    if (event.target.matches('button')) {
-        const key = event.target;
-        const action = key.dataset.action;
-        const content = key.textContent;
-        const displayOfCalculator = display.textContent;
-        
     }
 }
 
